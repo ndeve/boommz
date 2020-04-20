9faefb9bb50ff616b37b3edec791236f65d20025
ndeve <nicolas.deve@uni-medias.com>
2020-04-20 15:55:08 +0200

    boommz generator

---
+++ src/Form/PersonaCreatorType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PersonaCreatorType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+          ->add('persona', HiddenType::class, ['empty_data' => '/persona/creator/women/0000.png'])
+          ->add('hair', HiddenType::class)
+          ->add('eyes', HiddenType::class)
+          ->add('nose', HiddenType::class)
+          ->add('mouth', HiddenType::class)
+          ->add('hat', HiddenType::class)
+          ->add('top', HiddenType::class)
+          ->add('vest', HiddenType::class)
+          ->add('trousers', HiddenType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
