358867af5b366d66051c18609a1c8cca912b5acb
ndeve <nicolas.deve@uni-medias.com>
2020-04-24 00:43:31 +0200

    size screen

--- src/Command/ComicScreenCommand.php
+++ src/Command/ComicScreenCommand.php
@@ -55,8 +55,7 @@ class ComicScreenCommand extends Command
 
             $height = (count($comic->getPages()[0]->getBoxes())*260) + 210;
 
-            $command = 'node /home/wwwroot/boommz/assets/js/screen.js --url="'. $url
-              .'" --outputDir="'. $dir
+            $command = 'node /home/wwwroot/boommz/assets/js/screen.js --url="'. $url .'" --outputDir="'. $dir
               .'" --output="-'. $comic->getRewritten() .'-'. $comic->getId() .'.png"'
             .'--viewportHeight='. $height .' --viewportWidth=450';
             shell_exec($command);
