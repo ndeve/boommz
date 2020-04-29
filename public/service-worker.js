846db011e33c856d5c9aa75b32162cbfda3a6bd2
ndeve <nicolas.deve@uni-medias.com>
2020-04-29 21:26:35 +0200

    service-worker.js added

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,8 @@
 self.addEventListener('install', (event) => {
-    console.log('👷', 'install', event);
     self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-    console.log('👷', 'activate', event);
     return self.clients.claim();
 });
 
