a5c5c66d7a8521756695c8e0c47fbe32188bc0cd
ndeve <nicolas.deve@uni-medias.com>
2020-04-30 23:54:00 +0200

    clothes

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,17 +4,27 @@ const butInstall = document.getElementById('butInstall');
 /* Put code here */
 
 
-
 /* Only register a service worker if it's supported */
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/service-worker.js');
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     // Check for click events on the navbar burger icon
-    $(".navbar-burger").click(function() {
+    $(".navbar-burger").click(function () {
         $(".navbar-burger").toggleClass("is-active");
         $(".navbar-menu").toggleClass("is-active");
     });
+
+    if (navigator.share) {
+        $('.share').removeClass('is-hidden');
+        $('.share').on('click', function () {
+            navigator.share({
+                title: document.title,
+                url: document.location
+            });
+        });
+    }
+
 });
