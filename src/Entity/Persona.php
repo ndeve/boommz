e7580fe80b59325b561995499a5c9dfa4ca1bbce
ndeve <nicolas.deve@uni-medias.com>
2020-05-02 23:23:49 +0200

    head persona

--- src/Entity/Persona.php
+++ src/Entity/Persona.php
@@ -162,7 +162,7 @@ class Persona
             return '/persona/'. $this->getPath() . Transliterator::urlize($this->getName()) .'.png';
         }
 
-        return '/persona/creator/p/'. substr(str_replace('-', '/', $this->getPath()), 0, 15) . $this->getPath() .'.png';
+        return '/persona/creator/p/'. substr(str_replace('-', '/', $this->getPath()), 1, 15) . $this->getPath() .'.png';
     }
 
     public function getUrlHead()
@@ -171,7 +171,7 @@ class Persona
             return '/persona/'. $this->getPath() . Transliterator::urlize($this->getName()) .'.png';
         }
 
-        return '/persona/creator/p/'. substr(str_replace('-', '/', $this->getPath()), 0, 15) . 'head-'. $this->getPath() .'.png';
+        return '/persona/creator/p/'. substr(str_replace('-', '/', $this->getPath()), 1, 15) . 'head-'. $this->getPath() .'.png';
     }
 
     public function getPath(): ?string
