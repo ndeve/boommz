1f3c9dda5f84d3709f1cfa439f68f6f9fe69ec10
ndeve <nicolas.deve@uni-medias.com>
2020-05-07 22:31:15 +0200

    clothes

--- src/Form/PersonaCreatorType.php
+++ src/Form/PersonaCreatorType.php
@@ -13,13 +13,13 @@ class PersonaCreatorType extends AbstractType
     {
         $builder
           ->add('persona', HiddenType::class, ['empty_data' => '/persona/creator/women/0000.png'])
-          ->add('hair', HiddenType::class)
           ->add('eyes', HiddenType::class)
           ->add('nose', HiddenType::class)
           ->add('top', HiddenType::class)
           ->add('trousers', HiddenType::class)
           ->add('vest', HiddenType::class)
           ->add('mouth', HiddenType::class)
+          ->add('hair', HiddenType::class)
           ->add('hat', HiddenType::class)
         ;
     }
